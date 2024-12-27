1c9ba11fa1ebd86874f881d8617a435e989fd1ae
earl9rey <earl9rey@naver.com>
2024-12-28 03:23:14 +0900

    Feat: 스케줄 조정 페이지- 제목 헤더 바 적용

--- src/app/schedule/select/page.tsx
+++ src/app/schedule/select/page.tsx
@@ -1,7 +1,26 @@
-import React from 'react'
+'use client'
 
-const page = () => {
-  return <div>스케줄 셀렉트</div>
+import { useState } from 'react'
+import Title from '@/components/Header/Title' // 하위 컴포넌트
+
+export default function Page() {
+  const [title, setTitle] = useState('제목 없는 일정') // 제목 상태 관리
+  const isPurple = false
+
+  // 제목 수정 함수
+  const handleTitleChange = (newTitle: string) => {
+    setTitle(newTitle) // 수정된 제목으로 상태 업데이트
+  }
+
+  return (
+    <div>
+      <Title
+        buttonText="완료"
+        buttonLink="/"
+        initialTitle={title} // 하위 컴포넌트에 제목 전달
+        onTitleChange={handleTitleChange} // 제목 수정 함수 전달
+        isPurple={isPurple}
+      />
+    </div>
+  )
 }
-
-export default page
