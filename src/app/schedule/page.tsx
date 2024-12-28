4fefc379ea514f6febf1dc493804b5aa3d1d143a
solux <lee.yeji0327@gmail.com>
2024-12-28 23:32:32 +0900

    feat: /schedule 경로에 Button, Options, Modal,Calendar 추가

--- src/app/schedule/page.tsx
+++ src/app/schedule/page.tsx
@@ -1,7 +1,50 @@
-import React from 'react'
+'use client'
 
-const page = () => {
-  return <div>스케줄페이지</div>
+import React, { useState } from 'react'
+import { ScheduleOptions } from '@/components/Buttons/Floating/Options'
+import CustomModal from '@/components/Modals/CustomModal'
+import CustomCalendar from '@/components/CustomCalendar'
+import Button from '@/components/Buttons/Floating/Button'
+import { DateRange } from 'react-day-picker'
+
+export default function ScheduleLanding() {
+  const [isOpen, setIsOpen] = useState(false)
+  const [isDialogOpen, setIsDialogOpen] = useState(false)
+  const [selectedDate, setSelectedDate] = useState<DateRange>()
+  console.log('from', selectedDate?.from)
+  console.log('to', selectedDate?.to)
+
+  const handleToggle = () => {
+    setIsOpen(!isOpen)
+  }
+  const handleOpenDialog = () => {
+    setIsDialogOpen(!isDialogOpen)
+  }
+  return (
+    <div>
+      {/* Add Moim Button */}
+      <ScheduleOptions
+        isOpen={isOpen}
+        onClose={() => setIsOpen(false)}
+        optionStringUp="일정 조율하기"
+        optionStringDown="직접 입력하기"
+        onClickUp={handleOpenDialog}
+        onClickDown={() => alert('직접 입력하기 선택')}
+      />
+      <Button
+        buttonString="일정 추가하기"
+        onClick={handleToggle}
+        isOpen={isOpen}
+      />
+
+      {/* Dialog */}
+      <CustomModal
+        open={isDialogOpen}
+        onOpenChange={handleOpenDialog}
+        onNext={() => alert('다음으로')}
+      >
+        <CustomCalendar selected={selectedDate} onSelect={setSelectedDate} />
+      </CustomModal>
+    </div>
+  )
 }
-
-export default page
