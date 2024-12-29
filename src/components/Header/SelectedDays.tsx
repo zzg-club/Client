574c4bc9ab42c06b36e1f26a671d246d24d3603f
earl9rey <earl9rey@naver.com>
2024-12-29 15:43:40 +0900

    Feat: 캘린더 헤더 컴포넌트 생성

---
+++ src/components/Header/SelectedDays.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+export default function SelectedDays() {
+  return (
+    <div>
+      <div>12월</div>
+      <div className="flex items-center gap-1">
+        <p>1</p>
+        <p>2</p>
+      </div>
+    </div>
+  )
+}
