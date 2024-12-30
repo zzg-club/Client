3921b2a48b078219ff172e4f1b914329105e6a5b
ynzung <syj91105@sookmyung.ac.kr>
2024-12-30 22:08:17 +0900

    Feat: +장소 정하기 버튼 컴포넌트 구현

---
+++ src/components/Buttons/WhiteButtton.tsx
@@ -0,0 +1,20 @@
+export interface ButtonProps {
+  text: string
+  onClick?: () => void
+  className?: string
+}
+
+export function WhiteButton({ text, onClick, className = '' }: ButtonProps) {
+  return (
+    <div
+      className={`w-[125px] h-[35px] text-[#9562fa] rounded-3xl border-2 border-[#9562fa] justify-center items-center gap-3 inline-flex cursor-pointer ${className}`}
+    >
+      <button
+        onClick={onClick}
+        className={'text-sm text-center font-semibold leading-[17px]'}
+      >
+        + {text}
+      </button>
+    </div>
+  )
+}
