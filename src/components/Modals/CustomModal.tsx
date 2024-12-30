7adf2204fb20f4062ad984b1fcbe3db5b97a89d2
solux <lee.yeji0327@gmail.com>
2024-12-30 23:08:44 +0900

    feat: footertext props 추가

--- src/components/Modals/CustomModal.tsx
+++ src/components/Modals/CustomModal.tsx
@@ -12,6 +12,7 @@ interface ModalProps {
   children: ReactNode
   onNext?: () => void // '다음으로' 버튼 클릭 시 실행
   isFooter: boolean // 하단 버튼 이용 여부
+  footerText?: string // 하단 버튼 이용 시 버튼 텍스트
 }
 
 export default function CustomModal({
@@ -20,10 +21,11 @@ export default function CustomModal({
   children,
   onNext,
   isFooter,
+  footerText,
 }: ModalProps) {
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
-      <DialogContent className="bg-[#f5f6f8] rounded-3xl w-auto p-0 gap-0 [&>button]:hidden">
+      <DialogContent className="bg-[#f5f6f8] rounded-3xl w-80 p-0 gap-0 [&>button]:hidden">
         <div className="relative p-6">
           <DialogClose className="absolute right-6">
             <X></X>
@@ -37,7 +39,7 @@ export default function CustomModal({
               onClick={onNext}
               className="text-center text-[#9562fa] font-medium rounded-3xl text-lg py-5 w-full"
             >
-              다음으로
+              {footerText}
             </button>
           </DialogFooter>
         )}
