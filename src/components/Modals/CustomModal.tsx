df41b2e1c23ff75aa7c575974d28d028db4bd3bc
ynzung <syj91105@sookmyung.ac.kr>
2025-01-01 15:47:47 +0900

    Refactor: 커스텀모달 border 스타일 수정

--- src/components/Modals/CustomModal.tsx
+++ src/components/Modals/CustomModal.tsx
@@ -29,7 +29,7 @@ export default function CustomModal({
 }: ModalProps) {
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
-      <DialogContent className="bg-[#f5f6f8] rounded-3xl w-[280px] p-0 gap-0 [&>button]:hidden">
+      <DialogContent className="bg-[#f5f6f8] rounded-3xl w-[280px] p-0 gap-0 [&>button]:hidden border-0">
         <div className="relative p-6 w-[280px]">
           {isUserPlus && (
             <button
