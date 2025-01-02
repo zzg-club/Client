12a207d4783a14ab2fe222ac005d17f9b5fbd7c4
ynzung <syj91105@sookmyung.ac.kr>
2025-01-02 23:56:57 +0900

    Feat: DefaultNotification 컴포넌트 개발 완료

---
+++ src/components/Notification/DefaultNotification.tsx
@@ -0,0 +1,52 @@
+'use client'
+
+interface DefaultNotificationProps {
+  isVisible: boolean
+  onLeftBtn: () => void
+  onRightBtn: () => void
+  leftBtnText?: string // 확인 버튼 텍스트
+  RightBtnText?: string // 취소 버튼 텍스트
+  notiMessage?: string // 알림 메시지 텍스트
+}
+
+export default function DefaultNotification({
+  isVisible,
+  onLeftBtn,
+  onRightBtn,
+  leftBtnText = '이어서 하기', // 기본값
+  RightBtnText = '새로 만들기', // 기본값
+  notiMessage = '생성하던 일정이 있습니다!', // 기본값
+}: DefaultNotificationProps) {
+  if (!isVisible) return null
+
+  return (
+    <div className="w-full h-[119px] bg-[#9562fa] px-16 py-5 flex-col justify-center items-center gap-5 inline-flex">
+      <div className="flex-col justify-center items-center gap-5 flex">
+        {/* 메시지 텍스트 */}
+        <div className="text-white text-xl font-medium leading-[17px]">
+          {notiMessage}
+        </div>
+        <div className="justify-center items-center gap-6 inline-flex">
+          {/* 확인 버튼 */}
+          <button
+            onClick={onLeftBtn}
+            className="w-[120px] h-[42px] px-2.5 py-3 rounded-3xl border border-white justify-center items-center flex"
+          >
+            <div className="text-center text-white text-base font-normal font-['Pretendard'] leading-[17px]">
+              {leftBtnText}
+            </div>
+          </button>
+          {/* 취소 버튼 */}
+          <button
+            onClick={onRightBtn}
+            className="w-[120px] h-[42px] px-2.5 py-3 rounded-3xl border border-white justify-center items-center flex"
+          >
+            <div className="text-center text-white text-base font-normal font-['Pretendard'] leading-[17px]">
+              {RightBtnText}
+            </div>
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
