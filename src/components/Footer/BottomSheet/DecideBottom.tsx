65990489300cfca91da640e036682a1110e8d220
ynzung <syj91105@sookmyung.ac.kr>
2025-01-09 14:22:11 +0900

    Refactor: DecideBottom.tsx 접혀있을 때 높이 재설정

--- src/components/Footer/BottomSheet/DecideBottom.tsx
+++ src/components/Footer/BottomSheet/DecideBottom.tsx
@@ -47,8 +47,8 @@ const DecideBottom: React.FC<DecideBottomProps> = ({
     <AnimatePresence>
       <motion.div
         className="absolute bottom-0 w-full bg-white rounded-t-3xl shadow-[0px_0px_10px_0px_rgba(30,30,30,0.10)]"
-        initial={{ y: isOpen ? viewportHeight : '300px' }}
-        animate={{ y: isOpen ? 0 : '200px' }}
+        initial={{ y: isOpen ? viewportHeight : '500px' }}
+        animate={{ y: isOpen ? 0 : '500px' }}
         exit={{ y: viewportHeight }}
         transition={{ type: 'spring', damping: 30, stiffness: 300 }}
         drag="y"
