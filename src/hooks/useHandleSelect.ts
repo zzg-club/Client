14cb92ab425198d87a33eb68fb507e86caa1462e
solux <lee.yeji0327@gmail.com>
2025-01-15 01:47:39 +0900

    feat: ['yyyy-MM-dd', 'EEE'] 형태의 배열 리턴하도록 useHandleSelect 훅 변경

--- src/hooks/useHandleSelect.ts
+++ src/hooks/useHandleSelect.ts
@@ -2,29 +2,35 @@
 
 import { useState } from 'react'
 import { DateRange } from 'react-day-picker'
-import { eachDayOfInterval } from 'date-fns'
+import { eachDayOfInterval, format } from 'date-fns'
 import { useFormatDate } from './useFormatDate'
+
+type DateInfo = [string, string] // [yyyy-MM-dd, day of week]
 
 type UseHandleSelectReturn = {
   selectedDates: DateRange | Date[] | Date | undefined
-  stringDates: string[]
+  stringDates: DateInfo[]
   handleSelect: (selection: DateRange | Date[] | Date | undefined) => void
 }
 
-// 달력 날짜 선택 모드(single, range, multiple)에 따라 날짜 데이터 포맷 변경 후 배열을 반환하는 훅
 export function useHandleSelect(): UseHandleSelectReturn {
   const [selectedDates, setSelectedDates] = useState<
     DateRange | Date[] | Date | undefined
   >()
-  const [stringDates, setStringDates] = useState<string[]>([])
+  const [stringDates, setStringDates] = useState<DateInfo[]>([])
   const { formatDateToString } = useFormatDate()
+
+  const formatDateInfo = (date: Date): DateInfo => [
+    formatDateToString(date),
+    format(date, 'EEE').toLowerCase(),
+  ]
 
   const handleSelect = (selection: DateRange | Date[] | Date | undefined) => {
     setSelectedDates(selection)
     if (selection) {
       if (Array.isArray(selection)) {
         // multiple mode 처리
-        const formattedDates = selection.map(formatDateToString)
+        const formattedDates = selection.map(formatDateInfo)
         setStringDates(formattedDates)
         console.log('[multiple] 선택된 날짜:', formattedDates)
       } else if ('from' in selection && selection.from) {
@@ -34,21 +40,22 @@ export function useHandleSelect(): UseHandleSelectReturn {
             start: selection.from,
             end: selection.to,
           })
-          const formattedDates = dates.map(formatDateToString)
+          const formattedDates = dates.map(formatDateInfo)
           setStringDates(formattedDates)
           console.log('[range] 선택된 날짜:', formattedDates)
         } else {
-          const startDayOnlyString = formatDateToString(selection.from)
-          setStringDates([startDayOnlyString])
-          console.log('[range] 첫 날짜만 선택:', startDayOnlyString)
+          const startDayOnlyInfo = formatDateInfo(selection.from)
+          setStringDates([startDayOnlyInfo])
+          console.log('[range] 첫 날짜만 선택:', startDayOnlyInfo)
         }
       } else if (selection instanceof Date) {
         // single 모드 처리
-        const formattedDate = formatDateToString(selection)
+        const formattedDate = formatDateInfo(selection)
         setStringDates([formattedDate])
         console.log('[single] 선택된 날짜:', formattedDate)
       }
     } else {
+      setStringDates([])
       console.log('No date selected')
     }
   }
