d91ce0f72104de516bb25bdcf07b5627f76c5e21
ynzung <syj91105@sookmyung.ac.kr>
2025-01-22 22:25:34 +0900

    Feat: SelectedDays, editPage 연결

--- src/app/schedule/edit/page.tsx
+++ src/app/schedule/edit/page.tsx
@@ -7,6 +7,31 @@ import EditTimeStamp from '@/components/Body/Edit/EditTimeStamp'
 import SelectedBottom from '@/components/Footer/BottomSheet/SelectedBottom'
 import { EditItem } from '@/components/Footer/ListItem/EditItem'
 
+interface GroupedDate {
+  weekday: string
+  dates?: {
+    year: number
+    month: number
+    day: number
+    weekday: string
+  }[]
+  date?: {
+    year: number
+    month: number
+    day: number
+    weekday: string
+  }[]
+}
+
+interface ScheduleData {
+  name: string // 일정 이름
+  userId: number // 사용자 ID
+  groupId: number // 그룹 ID
+  mode: string
+  selected: string[] | null
+  date: [string, string][] // 날짜 배열: [날짜, 요일]의 배열
+}
+
 interface TimeSlot {
   slotId: number
   start: string
@@ -20,14 +45,15 @@ interface DateData {
 }
 
 interface SelectedDate {
-  date: number
-  weekday: string
+  year: number
   month: number
+  day: number
+  weekday: string
 }
 
 const mockSelectedSchedule: DateData[] = [
   {
-    date: '2025-01-01',
+    date: '2024-01-06',
     timeSlots: [
       {
         slotId: 1,
@@ -42,12 +68,12 @@ const mockSelectedSchedule: DateData[] = [
     ],
   },
   {
-    date: '2025-01-02',
+    date: '2024-02-08',
     timeSlots: [
       {
         slotId: 3,
-        start: '09:00',
-        end: '13:00',
+        start: '06:00',
+        end: '10:30',
       },
       {
         slotId: 4,
@@ -57,35 +83,306 @@ const mockSelectedSchedule: DateData[] = [
     ],
   },
   {
-    date: '2025-01-03',
+    date: '2024-01-13',
     timeSlots: [
       {
         slotId: 5,
+        start: '09:00',
+        end: '13:00',
+      },
+      {
+        slotId: 6,
+        start: '17:30',
+        end: '22:00',
+      },
+    ],
+  },
+  {
+    date: '2024-02-15',
+    timeSlots: [
+      {
+        slotId: 7,
         start: '04:30',
         end: '10:00',
       },
       {
-        slotId: 6,
+        slotId: 8,
         start: '13:30',
         end: '16:00',
       },
     ],
   },
   {
-    date: '2025-01-04',
+    date: '2024-01-20',
     timeSlots: [
       {
-        slotId: 7,
+        slotId: 9,
         start: '06:00',
         end: '10:30',
       },
       {
-        slotId: 8,
+        slotId: 10,
         start: '17:30',
         end: '22:00',
       },
     ],
   },
+  {
+    date: '2024-02-22',
+    timeSlots: [
+      {
+        slotId: 11,
+        start: '06:00',
+        end: '10:30',
+      },
+      {
+        slotId: 12,
+        start: '16:30',
+        end: '22:00',
+      },
+    ],
+  },
+  {
+    date: '2024-01-27',
+    timeSlots: [
+      {
+        slotId: 13,
+        start: '06:00',
+        end: '10:30',
+      },
+      {
+        slotId: 14,
+        start: '19:30',
+        end: '22:00',
+      },
+    ],
+  },
+  {
+    date: '2024-03-03',
+    timeSlots: [
+      {
+        slotId: 15,
+        start: '06:00',
+        end: '10:30',
+      },
+      {
+        slotId: 16,
+        start: '16:30',
+        end: '22:00',
+      },
+    ],
+  },
+  {
+    date: '2024-03-10',
+    timeSlots: [
+      {
+        slotId: 17,
+        start: '06:00',
+        end: '10:30',
+      },
+      {
+        slotId: 18,
+        start: '19:30',
+        end: '22:00',
+      },
+    ],
+  },
+  {
+    date: '2024-03-17',
+    timeSlots: [
+      {
+        slotId: 19,
+        start: '06:00',
+        end: '10:30',
+      },
+      {
+        slotId: 20,
+        start: '19:30',
+        end: '22:00',
+      },
+    ],
+  },
+  {
+    date: '2024-03-24',
+    timeSlots: [
+      {
+        slotId: 21,
+        start: '06:00',
+        end: '10:30',
+      },
+      {
+        slotId: 22,
+        start: '19:30',
+        end: '22:00',
+      },
+    ],
+  },
+  {
+    date: '2024-03-31',
+    timeSlots: [
+      {
+        slotId: 23,
+        start: '06:00',
+        end: '10:30',
+      },
+      {
+        slotId: 24,
+        start: '19:30',
+        end: '22:00',
+      },
+    ],
+  },
+  //range 데이터
+  // {
+  //   date: '2024-12-30',
+  //   timeSlots: [
+  //     {
+  //       slotId: 1,
+  //       start: '01:00',
+  //       end: '05:30',
+  //     },
+  //     {
+  //       slotId: 2,
+  //       start: '12:30',
+  //       end: '16:00',
+  //     },
+  //   ],
+  // },
+  // {
+  //   date: '2024-12-31',
+  //   timeSlots: [
+  //     {
+  //       slotId: 3,
+  //       start: '06:00',
+  //       end: '10:30',
+  //     },
+  //     {
+  //       slotId: 4,
+  //       start: '17:30',
+  //       end: '22:00',
+  //     },
+  //   ],
+  // },
+  // {
+  //   date: '2025-01-01',
+  //   timeSlots: [
+  //     {
+  //       slotId: 5,
+  //       start: '09:00',
+  //       end: '13:00',
+  //     },
+  //     {
+  //       slotId: 6,
+  //       start: '17:30',
+  //       end: '22:00',
+  //     },
+  //   ],
+  // },
+  // {
+  //   date: '2025-01-02',
+  //   timeSlots: [
+  //     {
+  //       slotId: 7,
+  //       start: '04:30',
+  //       end: '10:00',
+  //     },
+  //     {
+  //       slotId: 8,
+  //       start: '13:30',
+  //       end: '16:00',
+  //     },
+  //   ],
+  // },
+  // {
+  //   date: '2025-01-03',
+  //   timeSlots: [
+  //     {
+  //       slotId: 9,
+  //       start: '06:00',
+  //       end: '10:30',
+  //     },
+  //     {
+  //       slotId: 10,
+  //       start: '17:30',
+  //       end: '22:00',
+  //     },
+  //   ],
+  // },
+  // {
+  //   date: '2025-01-04',
+  //   timeSlots: [
+  //     {
+  //       slotId: 11,
+  //       start: '06:00',
+  //       end: '10:30',
+  //     },
+  //     {
+  //       slotId: 12,
+  //       start: '16:30',
+  //       end: '22:00',
+  //     },
+  //   ],
+  // },
+  // {
+  //   date: '2025-01-05',
+  //   timeSlots: [
+  //     {
+  //       slotId: 13,
+  //       start: '06:00',
+  //       end: '10:30',
+  //     },
+  //     {
+  //       slotId: 14,
+  //       start: '19:30',
+  //       end: '22:00',
+  //     },
+  //   ],
+  // },
+  // {
+  //   date: '2025-01-06',
+  //   timeSlots: [
+  //     {
+  //       slotId: 15,
+  //       start: '06:00',
+  //       end: '10:30',
+  //     },
+  //     {
+  //       slotId: 16,
+  //       start: '16:30',
+  //       end: '22:00',
+  //     },
+  //   ],
+  // },
+  // {
+  //   date: '2025-01-07',
+  //   timeSlots: [
+  //     {
+  //       slotId: 17,
+  //       start: '06:00',
+  //       end: '10:30',
+  //     },
+  //     {
+  //       slotId: 18,
+  //       start: '19:30',
+  //       end: '22:00',
+  //     },
+  //   ],
+  // },
+  // {
+  //   date: '2025-01-08',
+  //   timeSlots: [
+  //     {
+  //       slotId: 19,
+  //       start: '06:00',
+  //       end: '10:30',
+  //     },
+  //     {
+  //       slotId: 20,
+  //       start: '19:30',
+  //       end: '22:00',
+  //     },
+  //   ],
+  // },
 ]
 
 export default function SchedulePage() {
@@ -93,7 +390,7 @@ export default function SchedulePage() {
   const [title, setTitle] = useState('제목 없는 일정')
   const [isPurple, setIsPurple] = useState(false)
   const [dateTime, setDateTime] = useState<
-    { date: number; timeSlots: { start: string; end: string }[] }[]
+    { date: string; timeSlots: { start: string; end: string }[] }[]
   >([])
   const [highlightedCol, setHighlightedCol] = useState<number | null>(null)
   const [isOpen, setIsOpen] = useState(false)
@@ -110,7 +407,10 @@ export default function SchedulePage() {
     }[]
   >([])
 
-  const [selectedDates, setSelectedDates] = useState<SelectedDate[]>([])
+  const [dateCounts, setDateCounts] = useState<number[]>([])
+  const [groupedDate, setGroupedDate] = useState<GroupedDate[]>([])
+  // const [selectedDates, setSelectedDates] = useState<SelectedDate[]>([])
+  const [selectedEditDates, setSelectedEditDates] = useState<SelectedDate[]>([])
   const [selectedTimeInfo, setSelectedTimeInfo] = useState<{
     date: string
     startTime: string
@@ -118,8 +418,93 @@ export default function SchedulePage() {
     slotId: number
   } | null>(null)
 
+  const weekdayMap: { [key: string]: string } = {
+    mon: '월',
+    tue: '화',
+    wed: '수',
+    thu: '목',
+    fri: '금',
+    sat: '토',
+    sun: '일',
+  }
+
+  function convertToSelectedDates(
+    scheduleData: ScheduleData[],
+  ): SelectedDate[] {
+    return scheduleData.flatMap((schedule) =>
+      schedule.date.map(([fullDate, weekday]) => {
+        const [year, month, day] = fullDate.split('-').map(Number)
+        return {
+          year,
+          month,
+          day: day,
+          weekday: weekdayMap[weekday],
+        }
+      }),
+    )
+  }
+
+  const scheduleData: ScheduleData[] = [
+    {
+      name: '팀플 대면 모임',
+      userId: 2,
+      groupId: 1,
+      mode: 'week',
+      selected: ['mon', 'wed', 'fri'],
+      date: [
+        ['2024-01-06', 'mon'],
+        ['2024-02-08', 'wed'],
+        ['2024-01-13', 'mon'],
+        ['2024-02-15', 'wed'],
+        ['2024-01-20', 'mon'],
+        ['2024-02-22', 'wed'],
+        ['2024-01-27', 'mon'],
+        ['2024-03-03', 'fri'],
+        ['2024-03-10', 'fri'],
+        ['2024-03-17', 'fri'],
+        ['2024-03-24', 'fri'],
+        ['2024-03-31', 'fri'],
+      ],
+      // mode: 'range',
+      // selected: null,
+      // date: [
+      //   ['2024-12-30', 'mon'],
+      //   ['2024-12-31', 'tue'],
+      //   ['2025-01-01', 'wed'],
+      //   ['2025-01-02', 'thu'],
+      //   ['2025-01-03', 'fri'],
+      //   ['2025-01-04', 'sat'],
+      //   ['2025-01-05', 'sun'],
+      //   ['2025-01-06', 'mon'],
+      //   ['2025-01-07', 'tue'],
+      //   ['2025-01-08', 'wed'],
+      // ['2024-01-09', 'thu'],
+      // ['2024-01-10', 'fri'],
+      // ['2024-01-11', 'sat'],
+      // ['2024-01-12', 'sun'],
+      // ],
+    },
+  ]
+
+  const DAYS_PER_PAGE = 7
+  const highlightedIndex =
+    highlightedCol !== null
+      ? highlightedCol - currentPage * DAYS_PER_PAGE
+      : null
+
+  const selectedDates: SelectedDate[] = convertToSelectedDates(scheduleData)
+  const mode = scheduleData[0].mode
+  const dayofWeek = scheduleData[0].selected
+  const month =
+    mode === 'range'
+      ? currentPage === Math.floor((highlightedCol ?? 0) / DAYS_PER_PAGE)
+        ? `${selectedDates[highlightedCol ?? currentPage * DAYS_PER_PAGE]?.month}월`
+        : `${selectedDates[currentPage * DAYS_PER_PAGE]?.month}월`
+      : `${groupedDate[currentPage]?.date?.[highlightedIndex ?? 0]?.month ?? groupedDate[currentPage]?.date?.[0]?.month}월`
+
   const handlePageChange = (newPage: number) => {
     setCurrentPage(newPage)
+    setIsOpen(false)
   }
 
   // useEffect(() => {
@@ -147,10 +532,10 @@ export default function SchedulePage() {
   //   setUpdateData(updatedData)
   // }, [dateTime]) // dateTimeData가 변경될 때마다 호출
 
-  const getDateTime = (col: number, start: string, end: string) => {
+  const getDateTime = (date: string, start: string, end: string) => {
     setTimeout(() => {
       setDateTime((prev) => {
-        const existingDateIndex = prev.findIndex((item) => item.date === col)
+        const existingDateIndex = prev.findIndex((item) => item.date === date)
         let newEntry = null
 
         if (existingDateIndex !== -1) {
@@ -203,11 +588,11 @@ export default function SchedulePage() {
                 .padStart(2, '0')}`,
             }
             timeSlots.push(mergedSlot)
-            newEntry = { date: col, timeSlots: [mergedSlot] }
+            newEntry = { date: date, timeSlots: [mergedSlot] }
           } else {
             const newSlot = { start, end }
             timeSlots.push(newSlot)
-            newEntry = { date: col, timeSlots: [newSlot] }
+            newEntry = { date: date, timeSlots: [newSlot] }
           }
 
           timeSlots.sort((a, b) => toMinutes(a.start) - toMinutes(b.start))
@@ -215,7 +600,7 @@ export default function SchedulePage() {
           updated[existingDateIndex].timeSlots = timeSlots
           return updated
         } else {
-          newEntry = { date: col, timeSlots: [{ start, end }] }
+          newEntry = { date: date, timeSlots: [{ start, end }] }
           return [...prev, newEntry]
         }
       })
@@ -230,17 +615,18 @@ export default function SchedulePage() {
 
   const initializeSelectedDates = useCallback(() => {
     return mockSelectedSchedule.map((dateData) => ({
-      date: parseInt(dateData.date.split('-')[2]),
+      day: parseInt(dateData.date.split('-')[2]),
       weekday: new Date(dateData.date).toLocaleDateString('ko-KR', {
         weekday: 'short',
       }),
       month: parseInt(dateData.date.split('-')[1]),
+      year: parseInt(dateData.date.split('-')[0]),
     }))
   }, [])
 
   useEffect(() => {
     if (selectedDates.length === 0) {
-      setSelectedDates(initializeSelectedDates())
+      setSelectedEditDates(initializeSelectedDates())
     }
   }, [initializeSelectedDates, selectedDates])
 
@@ -274,7 +660,7 @@ export default function SchedulePage() {
     console.log(slotId)
 
     const deletedSlot = updateData.find((item) => item.slotId === slotId)
-    console.log(deletedSlot)
+    console.log('deletedSlot: ', deletedSlot)
 
     if (deletedSlot) {
       console.log(`${deletedSlot.slotId} 삭제`)
@@ -284,10 +670,57 @@ export default function SchedulePage() {
     }
   }
 
-  const handleSelectedCol = (colIndex: number) => {
-    setHighlightedCol(colIndex)
-    setIsOpen(true)
+  // const handleSelectedCol = (colIndex: number) => {
+  //   setHighlightedCol(colIndex)
+  //   setIsOpen(true)
+  // }
+
+  const handleDateCountsChange = (
+    counts: number[],
+    groupedData: GroupedDate[],
+  ) => {
+    setDateCounts(counts)
+    setGroupedDate(groupedData)
   }
+
+  const handleSelectedCol = useCallback(
+    (colIndex: number, rowIndex: number) => {
+      if (rowIndex === -1) {
+        setHighlightedCol(null)
+        return 0
+      }
+
+      const pairStartRow = Math.floor(rowIndex / 2) * 2
+      const pairEndRow = pairStartRow + 1
+
+      const getStartLabel = (rowIndex: number) => {
+        const hours = Math.floor(rowIndex / 2)
+        const minutes = (rowIndex % 2) * 30
+        const formattedHour = String(hours).padStart(2, '0')
+        const formattedMinute = String(minutes).padStart(2, '0')
+        return `${formattedHour}:${formattedMinute}`
+      }
+
+      const getEndLabel = (rowIndex: number) => {
+        const nextRowIndex = rowIndex + 1
+        const hours = Math.floor(nextRowIndex / 2)
+        const minutes = (nextRowIndex % 2) * 30
+        const formattedHour = String(hours).padStart(2, '0')
+        const formattedMinute = String(minutes).padStart(2, '0')
+        return `${formattedHour}:${formattedMinute}`
+      }
+
+      const DefaultStartTime = getStartLabel(pairStartRow)
+      const DefaultEndTime = getEndLabel(pairEndRow)
+
+      setStartTime(DefaultStartTime)
+      setEndTime(DefaultEndTime)
+
+      setHighlightedCol(colIndex)
+      setIsOpen(true)
+    },
+    [],
+  )
 
   const handleTimeSelect = (
     colIndex: number,
@@ -299,20 +732,21 @@ export default function SchedulePage() {
       const selectedDate = selectedDates[colIndex]
 
       // 수정된 데이터를 상태에 추가
-      setSelectedTimeInfo({
-        date: `${selectedDate.month}월 ${selectedDate.date}일`,
-        startTime,
-        endTime,
-        slotId,
-      })
+      // setSelectedTimeInfo({
+      //   date: `${selectedDate.month}월 ${selectedDate.day}일`,
+      //   startTime,
+      //   endTime,
+      //   slotId,
+      // })
 
       // 선택된 시간 정보 업데이트
       setSelectedTimeInfo({
-        date: `${selectedDate.month}월 ${selectedDate.date}일`,
+        date: `${selectedDate.month}월 ${selectedDate.day}일`,
         startTime,
         endTime,
         slotId,
       })
+      console.log(selectedDate)
     }
   }
 
@@ -357,18 +791,25 @@ export default function SchedulePage() {
       />
       <SelectedDays
         selectedDates={selectedDates}
-        month={'1월'}
+        month={month}
+        mode={mode}
+        dayofWeek={dayofWeek}
         currentPage={currentPage}
         onPageChange={handlePageChange}
         highlightedCol={highlightedCol}
+        onDateCountsChange={handleDateCountsChange}
       />
       <div className="flex-grow overflow-hidden mt-2">
         <EditTimeStamp
+          mode={mode}
+          dateCounts={dateCounts}
+          handleActiveTime={handleActiveTime}
           selectedDates={selectedDates}
           currentPage={currentPage}
           onPageChange={handlePageChange}
           handleSelectedCol={handleSelectedCol}
           getDateTime={getDateTime}
+          groupedDate={groupedDate}
           mockSelectedSchedule={mockSelectedSchedule}
           handleTimeSelect={handleTimeSelect}
           isBottomSheetOpen={isOpen}
@@ -376,9 +817,41 @@ export default function SchedulePage() {
       </div>
       <SelectedBottom isOpen={isOpen} onClose={() => setIsOpen(false)}>
         <div>
+          {/* <SelectItem
+            date={
+              highlightedCol !== null && highlightedIndex !== null
+                ? mode === 'range'
+                  ? `${selectedDates[highlightedCol]?.month}월 ${selectedDates[highlightedCol]?.day}일`
+                  : (() => {
+                      const month =
+                        groupedDate[currentPage]?.date?.[highlightedIndex]
+                          ?.month
+                      const day =
+                        groupedDate[currentPage]?.date?.[highlightedIndex]?.day
+                      return month && day ? `${month}월 ${day}일` : ''
+                    })()
+                : ''
+            }
+            startTime={`${startTime}`}
+            endTime={`${endTime}`}
+          /> */}
           <EditItem
             slotId={selectedTimeInfo?.slotId || 0}
-            date={selectedTimeInfo?.date || ''}
+            // date={selectedTimeInfo?.date || ''}
+            date={
+              highlightedCol !== null && highlightedIndex !== null
+                ? mode === 'range'
+                  ? `${selectedDates[highlightedCol]?.month}월 ${selectedDates[highlightedCol]?.day}일`
+                  : (() => {
+                      const month =
+                        groupedDate[currentPage]?.date?.[highlightedIndex]
+                          ?.month
+                      const day =
+                        groupedDate[currentPage]?.date?.[highlightedIndex]?.day
+                      return month && day ? `${month}월 ${day}일` : ''
+                    })()
+                : ''
+            }
             startTime={selectedTimeInfo?.startTime || ''}
             endTime={selectedTimeInfo?.endTime || ''}
             onDelete={(slotId) => handleDeleteSchedule(slotId)}
