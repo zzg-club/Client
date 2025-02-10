8f1a8bb0935b92d00e3b1dfafeb58d1538f72133
ynzung <syj91105@sookmyung.ac.kr>
2025-02-11 02:57:49 +0900

    Build: 카카오톡 프로필 이미지 도메인 허용 설정

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ['img1.kakaocdn.net', 't1.kakaocdn.net'],
+  },
 }
 
 export default nextConfig
