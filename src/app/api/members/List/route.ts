13fa33cacb6db2cd9e61b5c7e893ebfa9a96e099
ynzung <syj91105@sookmyung.ac.kr>
2025-02-12 00:39:14 +0900

    Feat: 스케줄 카드 api 연동 코드 작성

---
+++ src/app/api/members/List/route.ts
@@ -0,0 +1,76 @@
+import axios from 'axios'
+
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL
+
+export type Participant = {
+  id: number
+  name: string
+  image: string
+  type: string
+}
+
+export type Schedule = {
+  id: number
+  startDate: string
+  title: string
+  startTime: string
+  endTime: string
+  location?: string
+  participants: Participant[]
+}
+
+export const membersListApi = {
+  getMembers: async () => {
+    try {
+      const response = await axios.get(`${API_BASE_URL}/api/members/List`, {
+        withCredentials: true, // 쿠키 전송 허용
+      })
+      if (Array.isArray(response.data.data)) {
+        console.log('스케줄 정보:', response.data.data)
+        return response.data.data.map((schedule: Schedule) => ({
+          id: schedule.id,
+          startDate: schedule.startDate || '날짜 미정',
+          title: schedule.title,
+          startTime: schedule.startTime || '시간 미정',
+          endTime: schedule.endTime || '시간 미정',
+          location: schedule.location || '',
+          participants: schedule.participants || [],
+        }))
+      } else {
+        console.error('데이터 구조 에러:', response.data.data)
+        return []
+      }
+    } catch (error) {
+      console.error('스케줄 정보 불러오기 실패:', error)
+      return []
+    }
+  },
+}
+
+// export const scheduleListApi = async (): Promise<Schedule[]> => {
+//   try {
+//     const response = await axios.get(`${API_BASE_URL}/api/members/List`, {
+//       withCredentials: true, // 쿠키 전송 허용
+//     })
+
+//     console.log('스케줄 정보:', response.data.data)
+
+//     if (Array.isArray(response.data.data)) {
+//       return response.data.data.map((schedule: Schedule) => ({
+//         id: schedule.id,
+//         startDate: schedule.startDate || '날짜 미정',
+//         title: schedule.title,
+//         startTime: schedule.startTime || '시간 미정',
+//         endTime: schedule.endTime || '시간 미정',
+//         location: schedule.location || '',
+//         participants: schedule.participants || [],
+//       }))
+//     } else {
+//       console.error('데이터 구조 에러:', response.data.data)
+//       return []
+//     }
+//   } catch (error) {
+//     console.error('스케줄 정보 불러오기 실패:', error)
+//     return []
+//   }
+// }
