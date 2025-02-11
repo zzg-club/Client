625025a21d84c71351ef2eb3d8b5f6172a01d288
ynzung <syj91105@sookmyung.ac.kr>
2025-02-12 01:33:16 +0900

    Comment: 주석 삭제

--- src/app/api/members/List/route.ts
+++ src/app/api/members/List/route.ts
@@ -2,6 +2,7 @@ import axios from 'axios'
 
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL
 
+// /api/members/List 연동
 export type Participant = {
   id: number
   name: string
@@ -19,7 +20,8 @@ export type Schedule = {
   participants: Participant[]
 }
 
-export const membersListApi = {
+// /api/members/List 연동
+export const getmembersListApi = {
   getMembers: async () => {
     try {
       const response = await axios.get(`${API_BASE_URL}/api/members/List`, {
@@ -46,31 +48,3 @@ export const membersListApi = {
     }
   },
 }
-
-// export const scheduleListApi = async (): Promise<Schedule[]> => {
-//   try {
-//     const response = await axios.get(`${API_BASE_URL}/api/members/List`, {
-//       withCredentials: true, // 쿠키 전송 허용
-//     })
-
-//     console.log('스케줄 정보:', response.data.data)
-
-//     if (Array.isArray(response.data.data)) {
-//       return response.data.data.map((schedule: Schedule) => ({
-//         id: schedule.id,
-//         startDate: schedule.startDate || '날짜 미정',
-//         title: schedule.title,
-//         startTime: schedule.startTime || '시간 미정',
-//         endTime: schedule.endTime || '시간 미정',
-//         location: schedule.location || '',
-//         participants: schedule.participants || [],
-//       }))
-//     } else {
-//       console.error('데이터 구조 에러:', response.data.data)
-//       return []
-//     }
-//   } catch (error) {
-//     console.error('스케줄 정보 불러오기 실패:', error)
-//     return []
-//   }
-// }
