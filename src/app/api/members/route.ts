412a61d0eab8b6e48fc4cbc106ee41a1f455da4e
solux <lee.yeji0327@gmail.com>
2025-02-12 00:31:50 +0900

    chore: axios 적용

--- src/app/api/members/route.ts
+++ src/app/api/members/route.ts
@@ -1,20 +1,20 @@
+import axios from 'axios'
+
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL
 
 export const createGroupId = async () => {
   try {
     // 그룹 생성
-    const response = await fetch(`${API_BASE_URL}/api/members`, {
-      method: 'POST',
-      credentials: 'include', // 쿠키 전송을 위해 필요
-    })
-
-    if (!response.ok) {
-      throw new Error(`서버 에러: ${response.status}`)
-    }
-
-    const jsonData = await response.json()
-    const groupId = jsonData.data.groupId
-    console.log('그룹 ID:', jsonData.data.groupId)
+    const response = await axios.post(
+      `${API_BASE_URL}/api/members`,
+      {},
+      {
+        withCredentials: true, // 쿠키 전송을 위해 필요
+      },
+    )
+    
+    const groupId = await response.data.data.groupId
+    console.log('그룹 ID:', response.data.data.groupId)
 
     return groupId
   } catch (error) {
--- src/app/api/schedule/route.ts
+++ src/app/api/schedule/route.ts
@@ -1,3 +1,5 @@
+import axios from 'axios'
+
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL
 
 export const createDirectSchedule = async (
@@ -8,26 +10,22 @@ export const createDirectSchedule = async (
 ) => {
   console.log(groupId, name, startDate, endDate)
   try {
-    const response2 = await fetch(`${API_BASE_URL}/api/schedule`, {
-      method: 'POST',
-      credentials: 'include', // 쿠키 전송을 위해 필요
-      headers: {
-        'Content-Type': 'application/json', // JSON 형식 명시
-      },
-      body: JSON.stringify({
+    const response = await axios.post(
+      `${API_BASE_URL}/api/schedule`,
+      {
         groupId: groupId, // 첫 번째 요청에서 받은 그룹 ID 사용
         name: name,
         startDate: startDate,
         endDate: endDate,
-      }),
-    })
-
-    if (!response2.ok) {
-      throw new Error(`서버 에러: ${response2.status}`)
-    }
-
-    const data2 = await response2.json()
-    console.log('직접 일정 생성 성공', data2)
+      },
+      {
+        withCredentials: true, // 쿠키 전송을 위해 필요
+        headers: {
+          'Content-Type': 'application/json', // JSON 형식 명시
+        },
+      },
+    )
+    console.log('직접 일정 생성 성공', response)
   } catch (error) {
     console.log('직접 일정 생성 실패', error)
   }
