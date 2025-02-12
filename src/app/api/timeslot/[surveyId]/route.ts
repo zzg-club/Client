9178e829879e58de318a5678e64da28f8c2c7c1e
earl9rey <earl9rey@naver.com>
2025-02-12 23:49:55 +0900

    Add: 타임슬롯 api 연동 진행 중 (selectApi, headerApi)

--- src/app/api/timeslot/[surveyId]/route.ts
+++ src/app/api/timeslot/[surveyId]/route.ts
@@ -2,16 +2,49 @@ import axios from 'axios'
 
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL
 
+export type SelectedDate = {
+  year: number
+  month: number
+  day: number
+  weekday: string
+}
+
+export type GroupedDate = {
+  weekday: string
+  dates?: {
+    year: number
+    month: number
+    day: number
+    weekday: string
+  }[]
+  date?: {
+    year: number
+    month: number
+    day: number
+    weekday: string
+  }[]
+}
+
+export type ScheduleData = {
+  title: string // 일정 이름
+  userId: number // 사용자 ID
+  groupId: number // 그룹 ID
+  mode: string
+  selected: string[] | null
+  date: [string, string][] // 날짜 배열: [날짜, 요일]의 배열
+}
+
 export const selectApi = {
   createTimeSlot: async (
+    surveyId: number,
     slotDate: string,
     startTime: string,
     endTime: string,
   ) => {
-    console.log(slotDate, startTime, endTime)
+    console.log(slotDate, startTime, endTime, surveyId)
     try {
       const response = await axios.post(
-        `${API_BASE_URL}/api/timeslot/{surveyId}`,
+        `${API_BASE_URL}/api/timeslot/${surveyId}`,
         {
           slotDate: slotDate,
           startTime: startTime,
@@ -31,6 +64,27 @@ export const selectApi = {
   },
 }
 
-// export const headerApi = {
-//     getSelectedDays:
-// }
+export const headerApi = {
+  getSelectedDays: async (
+    surveyId: number,
+    success: boolean,
+    data: {
+      mode: string
+      selected: [string]
+    },
+  ) => {
+    console.log(success, data)
+    try {
+      const response = await axios.get(
+        `${API_BASE_URL}/api/timeslot/${surveyId}/edit/header`,
+        {
+          withCredentials: true, // 쿠키 전송 허용
+        },
+      )
+
+      console.log('헤더 날짜 정보 받아오기 성공', response)
+    } catch (error) {
+      console.log('헤더 날짜 정보 받아오기 실패', error)
+    }
+  },
+}
