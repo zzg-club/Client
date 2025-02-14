a02f21e3d625190cf0bb6cc81703817bcb651b35
solux <lee.yeji0327@gmail.com>
2025-02-14 13:44:56 +0900

    del: 필요없는 코드 삭제

--- src/components/Modals/MembersVariant.tsx
+++ src/components/Modals/MembersVariant.tsx
@@ -7,7 +7,6 @@ import ModalNotification from '../Notification/ModalNotification'
 import CustomModal from './CustomModal'
 import ScheduleSelectShareModal from './ScheduleSelectShareModal'
 import '../../styles/BottomSheet.css'
-//import { useScheduleStore } from '@/store/scheduleStore'
 
 export interface ModalProps {
   startDate: string
@@ -138,7 +137,7 @@ export default function MembersVariant({
         onOpenChange={handleOpenUserPlus}
         isFooter={false}
       >
-        <ScheduleSelectShareModal inviteUrl={`https://moim.team/`} />
+        <ScheduleSelectShareModal />
       </CustomModal>
     </div>
   )
