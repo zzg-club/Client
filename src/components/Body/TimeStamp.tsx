996837e4e696ee1b973ad99dc96b7164e9e3def3
earl9rey <earl9rey@naver.com>
2025-02-15 22:24:29 +0900

    Chore: 타임슬롯 생성 마우스 이벤트 버블링 문제 해결

--- src/components/Body/TimeStamp.tsx
+++ src/components/Body/TimeStamp.tsx
@@ -1040,7 +1040,8 @@ export default function TimeStamp({
                       {!cellStatus.isConfirmed && cellStatus.isStartCell && (
                         <div
                           className="absolute -top-[5px] left-[10%] w-2 h-2 border-[2px] border-[#9562fa] bg-white rounded-full cursor-move"
-                          onMouseDown={() => {
+                          onMouseDown={(event) => {
+                            event.stopPropagation() // 버블링 방지
                             handleMouseDown(
                               rowIndex,
                               colIndex,
@@ -1053,7 +1054,8 @@ export default function TimeStamp({
                       {!cellStatus.isConfirmed && cellStatus.isEndCell && (
                         <div
                           className="absolute -bottom-[5px] right-[10%] w-2 h-2 border-[2px] border-[#9562fa] bg-white rounded-full cursor-move "
-                          onMouseDown={() => {
+                          onMouseDown={(event) => {
+                            event.stopPropagation() // 버블링 방지
                             handleMouseDown(
                               rowIndex,
                               colIndex,
