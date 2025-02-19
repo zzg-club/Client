0d130edfa350185e5b2111ebfdc888cd515502ca
ynzung <syj91105@sookmyung.ac.kr>
2025-02-19 23:41:26 +0900

    Fix: 스케줄 직접입력 휠 모달 닫기 버튼 제거

--- src/components/Modals/CustomModal.tsx
+++ src/components/Modals/CustomModal.tsx
@@ -34,9 +34,11 @@ export default function CustomModal({
         <div
           className={cn('relative w-[280px]', contentPadding ? 'p-6' : 'p-0')}
         >
-          <DialogClose className="absolute right-6 focus:outline-none z-[3000]">
-            <X></X>
-          </DialogClose>
+          {contentPadding && (
+            <DialogClose className="absolute right-6 focus:outline-none z-[3000]">
+              <X></X>
+            </DialogClose>
+          )}
           <DialogTitle></DialogTitle>
           {children}
         </div>
